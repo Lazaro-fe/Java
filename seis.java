ec21b3914c5f57c6aaf4be94d1ee54e75a8303df
Lázaro <155861352+Lazaro-fe@users.noreply.github.com>
2026-02-19 21:50:57 +0000

    Salvando

---
+++ seis.java
@@ -0,0 +1,14 @@
+public class seis {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int op;
+        int num;
+
+        do{
+            System.out.println("===== Menu ======");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Calcular");
+            System.out.println("2 - ");
+        }
+    }
+}
