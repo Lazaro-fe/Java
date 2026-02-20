8323f914599038b9a38e2b6bed34ae11bdd97150
Lázaro <155861352+Lazaro-fe@users.noreply.github.com>
2026-02-20 16:28:25 +0000

    Salvando

--- seis.java
+++ seis.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class seis {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,7 +10,25 @@ public class seis {
             System.out.println("===== Menu ======");
             System.out.println("0 - Sair");
             System.out.println("1 - Calcular");
-            System.out.println("2 - ");
+            System.out.println("2 - Realizar Contagem Regressiva");
+            System.out.println("");
+            System.out.println("Informe a opção: ");
+
+            op = sc.nextInt();
+            switch ((op)) {
+                case 1:
+                    System.out.println("Digite um número");
+                    num = sc.nextInt();
+                    for(int i = 1; i <=10; i++){
+                        
+                    }
+                    break;
+                case 2:
+
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
