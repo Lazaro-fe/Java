f24785d8b88dd671184f02bfd5eacdc842205b8c
Lázaro <155861352+Lazaro-fe@users.noreply.github.com>
2026-03-05 02:19:05 +0000

    Salvando

--- Road_trip.java
+++ Road_trip.java
@@ -63,22 +63,114 @@ public class Road_trip {
                         break;
                     }
 
-                    for(int i = 0; i < quantidade; i++) {
-                        System.out.println("Número Ônibus ["+(i+1)+"]:");
+                    for(int j = 0; j < quantidade; j++) {
+                        System.out.println("Número Ônibus ["+(j+1)+"]:");
 
                         while (!sc.hasNextInt()) {
                             System.out.println("Digite um número válido: ");
                             sc.next();
                         }
+                        numeros_de_onibus[j] = sc.nextInt();
+                        sc.nextLine();
+
+                        total_de_reservas = 0;
+                        for(int i = 0; i < max_reservas; i++){
+                            nomes_passageiros[i] = null;
+                            onibus_reservado[i] = 0;
+                        }
+
+                        onibus_registrado = true;
+                        assentos_reservados = false;
+                        System.out.println("Frota registrada com sucesso!!");
+                        break;
                     }
 
-                    break;
                 case 2:
+
+                    if(!onibus_registrado){
+                        System.out.println("Registre o ônibus para seguir adiante!!");
+                        break;
+                    }
+
+                    for(int  i = 0; i < quantidade; i++){
+                        System.out.println("Informe a quantidade de assentos disponíveis para cada ônibus ["+(i+1)+"]: ");
+                        while (!sc.hasNextInt()) {
+                            System.out.println("Digite um número válido: ");
+                            sc.next();
+                        }
+                        numeros_de_assentos_disponiveis[i] = sc.nextInt();
+                        sc.nextLine();
+
+                        if (numeros_de_assentos_disponiveis[i] < 1) {
+                            System.out.println("Quantidade de assentos inválida!\nDeve ser maior que 0.");
+                            numeros_de_assentos_disponiveis[i] = 0;
+                            assentos_reservados = false;
+                            break;
+                        }
+
+                        assentos_reservados = true;
+                        System.out.println("Assentos registrados com sucesso!!");
+                    }
 
                     break;
                 case 3:
 
+                    if (!assentos_reservados || !onibus_registrado) {
+                        System.out.println("Registe os ônibus e os assentos disponíveis para seguir adiante!!");
+                        break;
+                    }
+
+                    if (total_de_reservas >= max_reservas) {
+                        System.out.println("Número máximo de" +max_reservas+ "reservas atingido!!");
+                        break;
+                    }
+
+                    System.out.println("Informe o número do ônibus para reserva:");
+                    while (!sc.hasNextInt()) {
+                        System.out.println("Digite um número válido: ");
+                        sc.next();
+                    }
+                    
+                    int numero_onibus = sc.nextInt();
+                    sc.nextLine();
+
+                    int indice_onibus = -1;
+                    for (int i = 0; i < quantidade; i++) {
+                        if (numeros_de_onibus[i] == numero_onibus) {
+                            indice_onibus = i;
+                            break;
+                        }
+                    }
+
+                    if (indice_onibus == -1) {
+                        System.out.println("Ônibus não encontrado!!");
+                        break;
+                    }
+
+                    if (numeros_de_assentos_disponiveis[indice_onibus] <= 0) {
+                        System.out.println("Não há assentos disponíveis para o ônibus selecionado!!");
+                        break;
+                    }
+
+                    String nome_passageiros;
+                    while (true) {
+                        System.out.println("Informe o nome do passageiro para reserva:");
+                        nome_passageiros = sc.nextLine().trim();
+
+                        if (nome_passageiros.isEmpty()) {
+                            System.out.println("O nome do passageiro não pode estar vazio!!\nTente Novamente!!");
+                        } else {
+                            break;
+                        }
+                    }
+
+                    nomes_passageiros[total_de_reservas] = nome_passageiros;
+                    onibus_reservado[total_de_reservas] = numero_onibus;
+                    total_de_reservas++;
+                    numeros_de_assentos_disponiveis[indice_onibus]--;
+                    System.out.println("Reserva realizado com sucesso!!!");
                     break;
+
                 case 4:
 
                     break;
